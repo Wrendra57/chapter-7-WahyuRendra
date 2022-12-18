4baa38b7d9c476bc3298e902e2c394f2c2749a62
Wrendra57 <wrendra57@gmail.com>
2022-12-19 00:55:32 +0700

    update

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Homepage, MulaiSewaPage } from "./Pages";
+import {
+  legacy_createStore as createStore,
+  compose,
+  applyMiddleware,
+} from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/sewamobil" element={<MulaiSewaPage />} />
+        {/* <Route path="/listcar" element={<ListCar />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} /> */}
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
